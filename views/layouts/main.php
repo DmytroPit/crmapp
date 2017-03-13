f1a974aa70180edf020db9c76693b2c4aaed04bf
unknown <DmitryPit@DmytroPit.CDB>
2017-03-13 14:12:37 +0200

    Add composer files

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,14 +5,27 @@
  * Date: 02.03.2017
  * Time: 12:25
  */
-?>
+use yii\helpers\Html;
 
-<!DOCTYPE html>
-    <html>
-<head>
-    <title>CRM</title>
-</head>
-<body>
-<?= $content; ?>
-</body>
-</html>
+\yii\bootstrap\BootstrapAsset::register($this);
+\yii\web\YiiAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+    <!DOCTYPE html>
+        <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>"/>
+        <title><?= Html::encode($this->title)?></title>
+        <?php $this->head() ?>
+        <?= Html::csrfMetaTags() ?>
+    </head>
+    <body>
+        <?php $this->beginDody() ?>
+            <div class="container">
+                <?= $content; ?>
+                <footer class="footer"><?= Yii::powered(); ?></footer>
+            </div>
+        <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage() ?>
