77d78359d3c9e3e0a79a97bea8ca8e5ad6182e91
unknown <DmitryPit@DmytroPit.CDB>
2017-03-14 17:00:22 +0200

    Hotfix SnowAssetBundle

--- assets/snowAssetsBundle.php
+++ assets/snowAssetsBundle.php
@@ -9,7 +9,11 @@
 namespace app\assets;
 
 
-class snowAssetsBundle
-{
+use yii\web\AssetBundle;
 
+class SnowAssetsBundle extends AssetBundle
+{
+    public $sourcePath = '@app/assets/snow';
+    public $css = ['snow.css'];
+    public $depends = ['app\\assets\\ApplicationUiAssetBundle'];
 }
